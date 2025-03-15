43cb92f456c8aa2d83eb3182916fbf6a173330e7
Gabryel-777git config --global user.email <e107ti@etec.sp.gov.br>
2025-03-14 22:14:26 -0300

    piadinhasss

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Como se sabe que o Estado é católico?
+// Porque tudo o que faz leva um terço.
+
+Console.Clear();
+Console.Write("Como se sabe que o Estado é católico?");
+Thread.Sleep (700); Console.Write(" ."); 
+Thread.Sleep (700); Console.Write("."); 
+Thread.Sleep (700); Console.Write("."); 
+Thread.Sleep (700); Console.WriteLine("."); 
+Console.WriteLine ("Porque tudo o que faz leva um terço.");
+
+Thread.Sleep (2000); Console.WriteLine("você ta aqui ainda");
+Thread.Sleep (1500); Console.WriteLine("Então você quer mais?");
+Thread.Sleep (1500); Console.WriteLine("Entâo la vai mais uma");
+
+Thread.Sleep (1500); Console.Write("Uma gorda foi para o hospital e perguntou:"); Console.WriteLine(" Doutor o que eu faço para emagrecer?");
+Thread.Sleep (1500); Console.Write("então o medico disse "); Console.WriteLine(" Basta você mover sua cabeça para a esquerda e para a direita consecutivamente");
+ Thread.Sleep (1500); Console.Write ("Então a mulher disse:");
+ Thread.Sleep (1500); Console.Write (" Ok doutor, porém quantas vezes eu faço isso?");   
+ Thread.Sleep (1500); Console.Write(" então o medico disse: "); Console.WriteLine(" Toda vez que te oferecerem comida");
